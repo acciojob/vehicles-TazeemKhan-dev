0466006f44b7c8d4447282583fe1718ed4026e00
TAZEEM KHAN <95047488+TazeemKhan-dev@users.noreply.github.com>
2026-03-30 16:00:10 +0530

    Add getters for currentSpeed and currentDirection

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -30,5 +30,12 @@ public class Vehicle {
 
         System.out.println("stop method called - The vehicle is stopped");
     }
+    public int getCurrentSpeed() {
+    return currentSpeed;
+}
+
+public int getCurrentDirection() {
+    return currentDirection;
+}
 
 }

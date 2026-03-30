e37d35401123aa73a694d1d02bf282562c5173b0
TAZEEM KHAN <95047488+TazeemKhan-dev@users.noreply.github.com>
2026-03-30 16:24:23 +0530

    Implement getter methods in Car class
    Added required getter methods for Car class.

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -10,23 +10,53 @@ public class Car extends Vehicle {
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-         super(name);  
+        super(name);
         this.wheels = wheels;
         this.doors = doors;
         this.gears = gears;
         this.isManual = isManual;
         this.type = type;
         this.seats = seats;
-        this.currentGear = 1; 
+        this.currentGear = 1;
     }
 
     public void changeGear(int newGear){
-
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
+    // ===== REQUIRED GETTERS =====
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
     }
 }
